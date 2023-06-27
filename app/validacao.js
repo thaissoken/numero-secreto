59618d9b70729a402bdb34161e840dc4a19e275a
THAIS_SOKEN <thais.soken@outlook.com>
2023-06-27 13:07:52 -0400

    first commit

---
+++ app/validacao.js
@@ -0,0 +1,71 @@
+const numeros = {
+    'zero zero': 0,
+    '00': 0,
+    '01': 1,
+    'um': 1,
+    'dois': 2,
+    'três': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10
+ }
+ 
+ const corrigeNumeros = (palavra) => {
+    for(let numero in numeros){
+       if(palavra === numero){
+          palavra = numeros[numero];   
+       }         
+    }
+    return palavra;
+ }
+
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if(numeroForMaiorOuMenoQueOValorPermitido(numero)){
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else if(numero < numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenoQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
